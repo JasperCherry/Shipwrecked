0ddbfcc369c5067b48ef9c099419bcce32ead2d4
JasperCherry <kacper.wisnicki@gmail.com>
2016-12-16 03:43:00 +0000

    first graphics
    ships, water and names

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
 var port = process.env.PORT || 8080;
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
